8dfaf6804fd985312c862ed30404e646731a5ccf
Thomas Hilzendegen <thomas.hilzendegen@thinktecture.com>
2015-02-23 15:19:06 +0100

    Some small fixes for css Back button added

--- src/client/app/controllers/detail.js
+++ src/client/app/controllers/detail.js
@@ -5,16 +5,21 @@
      * @constructor
      * @param $scope
      * @param $routeParams
+     * @param $location
      * @param {ProductsService} productsService
      * @param imageUrl
      */
-    function DetailController($scope, $routeParams, productsService, imageUrl) {
+    function DetailController($scope, $routeParams, $location, productsService, imageUrl) {
         var id = $routeParams.id;
         productsService.getProduct(id)
-            .then(function(response){
+            .then(function (response) {
                 $scope.product = response.data;
                 $scope.image = imageUrl + $scope.product.ImageUrl;
             });
+
+        $scope.back = function () {
+            $location.path('/');
+        };
     }
 
     app.module.controller('detailController', DetailController);
