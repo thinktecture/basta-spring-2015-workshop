1b36cceb02a15de81dcc1d9526c6909de6276efb
Thomas Hilzendegen <thomas.hilzendegen@thinktecture.com>
2015-02-23 16:40:04 +0100

    Added cordova project including gulp tasks

--- src/client/gulpfile.js
+++ src/client/gulpfile.js
@@ -1,4 +1,5 @@
 var gulp = require('gulp'),
+    del = require('del'),
     concat = require('gulp-concat'),
     sourcemaps = require('gulp-sourcemaps'),
     uglify = require('gulp-uglify'),
@@ -16,6 +17,21 @@ gulp.task("scripts", function () {
 
 gulp.task("watch", function () {
     gulp.watch('app/**/*.js', ["scripts"]);
+});
+
+gulp.task("clean_cordova", function(cb){
+    del("cordova/www", cb);
+});
+
+gulp.task("cordova", ["clean_cordova"], function () {
+    gulp.src("app/**/*.html")
+        .pipe(gulp.dest("cordova/www/app"));
+    gulp.src("bower_components/**")
+        .pipe(gulp.dest("cordova/www/bower_components"));
+    gulp.src("build/**")
+        .pipe(gulp.dest("cordova/www/build"));
+    gulp.src("index.html")
+        .pipe(gulp.dest("cordova/www"));
 });
 
 gulp.task("default", ["scripts", "watch"], function () {
